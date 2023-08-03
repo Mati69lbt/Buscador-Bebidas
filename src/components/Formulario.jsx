7c683b3984bf85d5750131ac444372a8dd5fc6d9
Mati69lbt <mati69_lbt@hotmail.com>
2023-08-03 16:04:16 -0300

    primeros

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,11 @@
 import { Col, Form, Row } from "react-bootstrap";
+import useCategorias from "../hooks/useCategorias";
 
 const Formulario = () => {
+  const { categorias } = useCategorias();
+
+  console.log(categorias);
+
   return (
     <Form>
       <Row>
@@ -19,7 +24,16 @@ const Formulario = () => {
           <Form.Group className="mb-3">
             <Form.Label>Categoria Bebida</Form.Label>
             <Form.Select id="categoria" name="categoria">
-              <option value="">Seleccione una Categoria...</option>
+              {/* Opcion, selecciona categoria */}
+              <option>- Selecciona Categoria</option>
+              {categorias?.map((categoria) => (
+                <option
+                  key={categoria.strCategory}
+                  value={categoria.strCategory}
+                >
+                  {categoria.strCategory}
+                </option>
+              ))}
             </Form.Select>
           </Form.Group>
         </Col>
--- src/context/CategoriasProvider.jsx
+++ src/context/CategoriasProvider.jsx
@@ -1,11 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 const CategoriasContext = createContext();
 
 const CategoriasProvider = ({ children }) => {
+  const [categorias, setCategorias] = useState([]);
+
+  const obtenerCategorias = async () => {
+    try {
+      const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+
+      const { data } = await axios(url);
+
+      setCategorias(data.drinks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    obtenerCategorias();
+  }, []);
+
   return (
-    <CategoriasContext.Provider value={{}}>
+    <CategoriasContext.Provider value={{ categorias }}>
       {children}
     </CategoriasContext.Provider>
   );
